13134c2d378f27001b74b570b22a2146c37039fd
abc <mauriziobusi70@gmail.com>
2019-01-01 16:42:32 +0100

    non salvato

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
-
 const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -30,7 +32,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.render('maintenance.hbs');
 });
 
@@ -65,6 +67,6 @@ app.get('/bad', (req, res) =>{
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
